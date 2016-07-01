6b426bae04d2d830d56bc0165654da78fe077b17
Eli Levi GONZALEZ <elilevi.gonzalez@gmail.com>
2016-07-01 03:36:34 +0200

    Added app id and secret

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({ appId: 'telebot_contest_v1', appSecret: 'f926722a460c4f8c9a90c472f8e66f45' });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
